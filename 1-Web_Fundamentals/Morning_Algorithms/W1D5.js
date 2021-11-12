941bc8d5e0873c2ad2dbc74711c7ed65e6dd189b
Maria McBride <mariacmcbride2@gmail.com>
2021-11-12 11:25:37 -0800

    initial commit

--- 1-Web_Fundamentals/Morning_Algorithms/W1D5.js
+++ 1-Web_Fundamentals/Morning_Algorithms/W1D5.js
@@ -1,2 +1,12 @@
 // Write a function reverse(arr) to reverse an array:
 // ["a", "b", "c", "d", "e"]; → ["e", "d", "c", "b", "a"];
+
+function reverseArray(arr) {
+  var revArr = []; // create new variable representing the reversed array
+  for (var i = 0; i < arr.length; i++) {
+    revArr[i] = arr[arr.length - i - 1]; // update value of revArr to the last value of the old array
+  }
+  return revArr;
+}
+
+console.log(reverseArray(["a", "b", "c", "d", "e"]));
