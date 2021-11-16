3151215bbe72e18d58ca11baedba2dd605eb1c9c
Maria McBride <mariacmcbride2@gmail.com>
2021-11-15 20:57:22 -0800

    initial commit

---
+++ 1-Web_Fundamentals/W2D1/Button_Clicker/script.js
@@ -0,0 +1,14 @@
+// Change Login to Logout when clicked
+function logout(element) {
+  element.innerText = "Logout";
+}
+
+// Remove Add Definition when clicked
+function hide(element) {
+  element.remove();
+}
+
+// Alert pop-up when clicking like button
+function likedNinja() {
+  alert("Ninja was liked");
+}
