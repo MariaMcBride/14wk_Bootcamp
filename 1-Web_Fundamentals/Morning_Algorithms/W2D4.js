74046463f1db288eae4cd7a5dcdd4fa8fcc641cc
Maria McBride <mariacmcbride2@gmail.com>
2021-11-18 10:21:06 -0800

    updated assignment

--- 1-Web_Fundamentals/Morning_Algorithms/W2D4.js
+++ 1-Web_Fundamentals/Morning_Algorithms/W2D4.js
@@ -16,15 +16,38 @@ console.log(arr2d[0][2]);
 
 Note - Don't assume the array will always be the same size, we must rely on its .length property
 
-Hint - Can we put a for loop inside another for loop?
+Hint - Can we put a for loop inside another for loop? */
 
-Flatten Array
+var arr2d = [
+  [2, 5, 8],
+  [3, 6, 1],
+  [5, 7, 7],
+];
+
+function isPresent2d(arr2d, value) {
+  for (var i = 0; i < arr2d.length; i++) {
+    for (var j = 0; j < arr2d[i].length; j++) {
+      if (arr2d[i][j] == value) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+console.log(isPresent2d(arr2d, 5));
+
+/* Flatten Array
 Given a 2 dimensional array (an array containing arrays), return a new array containing just the values from inside the sub-arrays. Don't assume the array will always be the same size, or that the sub-arrays are all the same length (the array might be jagged). Don't use built-in methods like Array.prototype.flat() but feel free to use .push(value) and/or .pop() where needed. */
 
 // complete the following function
 function flatten(arr2d) {
   var flat = [];
-  // your code here
+  for (var i = 0; i < arr2d.length; i++) {
+    for (var j = 0; j < arr2d[i].length; j++) {
+      flat.push(arr2d[i][j]);
+    }
+  }
   return flat;
 }
 
@@ -34,3 +57,48 @@ var result = flatten([
   [5, 7, 7],
 ]);
 console.log(result); // we expect to get back [2, 5, 8, 3, 6, 1, 5, 7, 7]
+
+// Find a random index
+var arr2d = [
+  [2, 5, 8],
+  [3, 6, 1],
+  [5, 7, 7],
+];
+
+function randomIndex(arr2d) {
+  for (var i = 0; i < arr2d.length; i++) {
+    return arr2d[Math.floor(Math.random() * arr2d[i].length)];
+  }
+}
+
+console.log(randomIndex(arr2d));
+
+// Create a function that takes in an array
+// Create a new variable to hold the new array
+// Loop through the array that was passed
+// Check if the current index we are on is an array
+// If true, concat single level array onto the new array
+// If false, call function again
+// Push the current index's value to the new array
+// Return new array
+// Create variable result set to function with nested array
+// Print result variable
+
+function flatten(arr2d) {
+  var flat = [];
+  for (var i = 0; i < arr2d.length; i++) {
+    if (Array.isArray(arr2d[i])) {
+      flat = flat.concat(flatten(arr2d[i]));
+    } else {
+      flat.push(arr2d[i]);
+    }
+  }
+  return flat;
+}
+
+var result = flatten([
+  [2, 5, 8],
+  [3, 6, 1],
+  [5, 7, 7],
+]);
+console.log(result);
