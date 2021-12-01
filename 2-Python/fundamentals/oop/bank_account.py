9eede7901ef56a41b0148aab7e89d5c4dad0df60
Maria McBride <mariacmcbride2@gmail.com>
2021-11-30 20:02:56 -0800

    updated assignment

--- 2-Python/fundamentals/oop/bank_account.py
+++ 2-Python/fundamentals/oop/bank_account.py
@@ -1,17 +1,20 @@
 class BankAccount:
+    all_accounts = []
     def __init__(self, int_rate, balance): 
         self.int_rate = int_rate
         self.balance = balance
+        BankAccount.all_accounts.append(self)
         
     def deposit(self, amount):
         self.balance += amount
         return self
 
     def withdraw(self, amount):
-        self.balance -= amount
-        if self.balance == 0:
-            self.balance -= 5
+        if self.balance - amount >= 0:
+            self.balance -= amount
+        else:
             print(f"Insufficient funds: Charging a $5 fee")
+            self.balance -= 5
         return self
 
     def display_account_info(self):
@@ -21,9 +24,12 @@ class BankAccount:
     def yield_interest(self):
         if self.balance > 0:
             self.balance += self.balance * self.int_rate
-        else:
-            return False
         return self
+    
+    @classmethod
+    def print_all_accounts_info(cls):
+        for account in cls.all_accounts:
+            account.display_account_info()
 
 acct_1 = BankAccount(0.50, 5000)
 acct_2 = BankAccount(0.25, 1850)
@@ -31,3 +37,5 @@ acct_2 = BankAccount(0.25, 1850)
 acct_1.deposit(1000).deposit(1000).deposit(1000).withdraw(500).yield_interest().display_account_info()
 
 acct_2.deposit(500).deposit(700).withdraw(1500).withdraw(800).yield_interest().display_account_info()
+
+BankAccount.print_all_accounts_info()
