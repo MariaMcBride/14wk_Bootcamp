713408c6e6bb1ba97ca105e8d59a54d549a8434a
Maria McBride <mariacmcbride2@gmail.com>
2021-12-05 17:53:52 -0800

    initial commit

---
+++ 2-Python/flask/fundamentals/counter/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, redirect, session
+
+app = Flask(__name__)    
+app.secret_key = "full_counter"
+
+@app.route('/')          
+def index():
+    if 'visits' not in session:
+        session['visits'] = 0
+    else:
+        session['visits'] += 1
+    return render_template("index.html")
+
+@app.route('/destroy_session/')
+def clear_session():
+    session.clear()
+    return redirect('/')
+
+if __name__=="__main__":    
+    app.run(debug=True)
