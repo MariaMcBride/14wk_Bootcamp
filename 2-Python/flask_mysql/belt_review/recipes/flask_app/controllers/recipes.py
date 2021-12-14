514bcf4466e0578ad99d150edd5e1003b3cfba84
Maria McBride <mariacmcbride2@gmail.com>
2021-12-13 17:55:32 -0800

    updated assignment

--- 2-Python/flask_mysql/belt_review/recipes/flask_app/controllers/recipes.py
+++ 2-Python/flask_mysql/belt_review/recipes/flask_app/controllers/recipes.py
@@ -7,10 +7,7 @@ from flask_app.models.user import User
 def new_recipe():
     if 'user_id' not in session:
         return redirect('/logout')
-    data = {
-        "id":session['user_id']
-    }
-    return render_template('new_recipe.html', user = User.get_by_id(data))
+    return render_template('new_recipe.html', user = User.get_by_id(id=session['user_id']))
 
 @app.post('/recipes/create')
 def create_recipe():
@@ -18,30 +15,20 @@ def create_recipe():
         return redirect('/logout')
     if not Recipe.validate_recipe(request.form):
         return redirect('/recipe/new')
-    data = {
-        **request.form,
-        "user_id": session['user_id']
-    }
-    Recipe.add_recipe(data)
+    Recipe.add_recipe(**request.form, user_id=session['user_id'])
     return redirect('/dashboard')
 
 @app.route('/recipes/<int:id>')
 def view_recipe(id):
     if 'user_id' not in session:
         return redirect('/logout')
-    data = {
-        "id":session['user_id']
-    }
-    return render_template("view_recipe.html", user = User.get_by_id(data), recipe = Recipe.get_one(id=id))
+    return render_template("view_recipe.html", user = User.get_by_id(id=session['user_id']), recipe = Recipe.get_one(id=id))
 
 @app.route('/recipes/edit/<int:id>')
 def edit_recipe(id):
     if 'user_id' not in session:
         return redirect('/logout')
-    data = {
-        "id":session['user_id']
-    }
-    return render_template("edit_recipe.html", user = User.get_by_id(data), recipe = Recipe.get_one(id=id))
+    return render_template("edit_recipe.html", user = User.get_by_id(id=session['user_id']), recipe = Recipe.get_one(id=id))
 
 @app.post('/recipes/update/<int:id>')
 def update_recipe(id):
