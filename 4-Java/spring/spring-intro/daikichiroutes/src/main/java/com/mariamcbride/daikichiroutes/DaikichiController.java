955bdd1517243a0c0afebec5599f7eec637ee822
Maria McBride <mariacmcbride2@gmail.com>
2022-02-10 20:08:38 -0800

    added 2 routes for Daikichi Path Variables

--- 4-Java/spring/spring-intro/daikichiroutes/src/main/java/com/mariamcbride/daikichiroutes/DaikichiController.java
+++ 4-Java/spring/spring-intro/daikichiroutes/src/main/java/com/mariamcbride/daikichiroutes/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.mariamcbride.daikichiroutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,20 @@ public class DaikichiController {
 	@RequestMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@RequestMapping("/travel/{city}")
+	public String travel(@PathVariable("city") String city) {
+		return "Congratulations! You will soon travel to " + city + "!";
+	}
+	
+	@RequestMapping("/lotto/{number}")
+	public String lottoNumber(@PathVariable("number") String number) {
+		int num = Integer.parseInt(number);
+		if (num % 2 == 0) 
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		else 
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
 	}
 
 }
